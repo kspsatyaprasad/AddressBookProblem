14d8d5851221ca00c94737b0b1871432432cac12
kspsatyaprasad <koppulasatyaprasad@gmail.com>
2020-11-05 13:32:29 +0530

    added code to create a file without taking filename from user

--- AddressBookProblem.java
+++ AddressBookProblem.java
@@ -1,9 +1,28 @@
-public class AddresBookProblem
+import java.io.File;
+import java.io.IOException;
+public class Testing 
 {
-    public static void main(String args[])
-    {
 
-     System.out.println("Welcome to Address Book Problem Using JAVA");
+	public static void main(String[] args) 
+	{
+		try 
+		{
+			File myObj = new File("C:\\Users\\user\\Desktop\\AddressBook\\NewFile.txt");
+			if (myObj.createNewFile())
+			{
+				System.out.println("File created: "+ myObj.getName());
 
-    }
-} 
+			}  else 
+			{
+				System.out.println("File already exists.");
+			}
+		} 
+		catch (IOException e) 
+		{
+			System.out.println("An error occured.");
+			e.printStackTrace();
+		}
+		
+
+		}
+}
