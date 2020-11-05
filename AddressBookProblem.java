43e81d0373fa6ef05c54adf6d71360f3cab3832b
kspsatyaprasad <koppulasatyaprasad@gmail.com>
2020-11-05 13:53:45 +0530

    added code for reading a file without taking filename from user

--- AddressBookProblem.java
+++ AddressBookProblem.java
@@ -1,69 +1,20 @@
-import java.io.File;
-import java.io.IOException;
-import java.util.Scanner;
-public class CreateFileimport java.io.File;
-import java.io.IOException;
-import java.util.Scanner;
-public class CreateFile 
-{
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
 
-	public static void main(String[] args) 
-	{
-		try 
-		{   Scanner scanner = new Scanner(System.in);
-		    System.out.print("Enter file name: ");
-     		String fileName = scanner.next();
-    		File myObj = new File("C:\\Users\\user\\Desktop\\AddressBook\\"+fileName);
-			if (myObj.createNewFile())
-			{
-				System.out.println("File created: "+ myObj.getName());
-
-			}  else 
-			{
-				System.out.println("File already exists.");
-			}
-			
-					System.out.println("File name: "+myObj.getName());
-					System.out.println("AbsolutePath: "+myObj.getAbsolutePath());
-					System.out.println("Writable: "+myObj.canWrite());
-					System.out.println("Readable: "+myObj.canRead());
-					System.out.println("File size in Bytes: "+myObj.length());
-
-		     
-		} 
-		catch (IOException e) 
-		{
-			System.out.println("An error occured.");
-			e.printStackTrace();
-		}
-		
-
-		}
-} 
-{
-
-	public static void main(String[] args) 
-	{
-		try 
-		{   Scanner scanner = new Scanner(System.in);
-		    System.out.print("Enter file name: ");
-     		String fileName = scanner.next();
-    		File myObj = new File("C:\\Users\\user\\Desktop\\AddressBook\\"+fileName);
-			if (myObj.createNewFile())
-			{
-				System.out.println("File created: "+ myObj.getName());
-
-			}  else 
-			{
-				System.out.println("File already exists.");
-			}
-		} 
-		catch (IOException e) 
-		{
-			System.out.println("An error occured.");
-			e.printStackTrace();
-		}
-		
-
-		}
+public class ReadFile {
+  public static void main(String[] args) {
+    try {
+	  File myObj = new File("C:\\Users\\user\\Desktop\\AddressBook\\NewFile.txt");
+      Scanner myReader = new Scanner(myObj);
+      while (myReader.hasNextLine()) {
+        String data = myReader.nextLine();
+        System.out.println(data);
+      }
+      myReader.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+  }
 }
